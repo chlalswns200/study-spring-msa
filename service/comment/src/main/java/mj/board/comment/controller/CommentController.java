1a99688f397e4ba7a17a9ecb7b064329b3ae0a76
MinJunChoi <chlalswns200@naver.com>
2026-08-28 16:04:53 +0900

    feat: 댓글 조회 페이지 지정, 무한스크롤 기능 컨트롤러 추가

--- service/comment/src/main/java/mj/board/comment/controller/CommentController.java
+++ service/comment/src/main/java/mj/board/comment/controller/CommentController.java
@@ -3,8 +3,11 @@ package mj.board.comment.controller;
 import lombok.RequiredArgsConstructor;
 import mj.board.comment.service.CommentService;
 import mj.board.comment.service.request.CommentCreateRequest;
+import mj.board.comment.service.response.CommentPageResponse;
 import mj.board.comment.service.response.CommentResponse;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +27,25 @@ public class CommentController {
     @DeleteMapping("/v1/comments/{commentId}")
     public void delete (@PathVariable Long commentId) {
         commentService.delete(commentId);
+    }
+
+    @GetMapping("/v1/comments")
+    public CommentPageResponse readAll(
+            @PathVariable("aritcleId") Long articleId,
+            @PathVariable("page") Long page,
+            @PathVariable("pageSize") Long pageSize
+    ) {
+        return commentService.readAll(articleId, page, pageSize);
+    }
+
+    @GetMapping("/v1/comments/infinite-scroll")
+    public List<CommentResponse> readAll(
+            @PathVariable("aritcleId") Long articleId,
+            @PathVariable("lastParentCommentId") Long lastParentCommentId,
+            @PathVariable("lastCommentId") Long lastCommentId,
+            @PathVariable("pageSize") Long pageSize
+    ) {
+        return commentService.readAll(articleId, lastParentCommentId, lastCommentId, pageSize);
     }
 
 }
