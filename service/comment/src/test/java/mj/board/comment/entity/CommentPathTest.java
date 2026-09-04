aaa531c9f3dd07111b3c94a535eb35809af99ce9
MinJunChoi <chlalswns200@naver.com>
2026-09-04 16:46:12 +0900

    test: comment path 테스트 추가

--- service/comment/src/test/java/mj/board/comment/entity/CommentPathTest.java
+++ service/comment/src/test/java/mj/board/comment/entity/CommentPathTest.java
@@ -1,7 +1,55 @@
 package mj.board.comment.entity;
 
+import org.junit.jupiter.api.Test;
+
+import java.net.CookieHandler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommentPathTest {
+
+    @Test
+    void createChildCommentTest() {
+        // ----- <- 생성
+        createChildCommentTest(CommentPath.create(""), null, "00000");
+
+        // 00000
+        //       00000 <- 생성
+        createChildCommentTest(CommentPath.create("00000"), null, "0000000000");
+
+        // 00000
+        // 00001 <- 생성
+        createChildCommentTest(CommentPath.create(""), "00000", "00001");
+
+        // 0000z
+        //        abcdz
+        //              zzzzz
+        //                   zzzzzz
+        //        abce0 <- 생성
+        createChildCommentTest(CommentPath.create("0000z"),"0000zabcdzzzzzzzzzzz","0000zabce0");
+    }
+
+    void createChildCommentTest(CommentPath commentPath, String descendantsTopPath, String expectedChildPath) {
+        CommentPath childCommentPath = commentPath.createChildCommentPath(descendantsTopPath);
+        assertThat(childCommentPath.getPath()).isEqualTo(expectedChildPath);
+    }
+
+    @Test
+    void createChildPathMaxDepthTest() {
+        assertThatThrownBy(() ->
+                CommentPath.create("zzzzz".repeat(5)).createChildCommentPath(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void createChildCommentPathIfChunkOverflowTest() {
+        CommentPath commentPath = CommentPath.create("");
+
+        assertThatThrownBy(() ->
+                commentPath.createChildCommentPath("zzzzz"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
 }
