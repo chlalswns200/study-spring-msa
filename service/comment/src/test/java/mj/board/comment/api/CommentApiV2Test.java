5397631593be287f37c1d9dabfc45863320fc5f8
MinJunChoi <chlalswns200@naver.com>
2026-09-15 22:38:42 +0900

    test: comment v2 CUD 기능 추가

---
+++ service/comment/src/test/java/mj/board/comment/api/CommentApiV2Test.java
@@ -0,0 +1,59 @@
+package mj.board.comment.api;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import mj.board.comment.service.response.CommentResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+public class CommentApiV2Test {
+    RestClient restClient = RestClient.create("http://localhost:9001");
+
+    @Test
+    void create() {
+        CommentResponse response1 = create(new CommentCreateRequestV2(1L, "my comment1", null, 1L));
+        CommentResponse response2 = create(new CommentCreateRequestV2(1L, "my comment2", response1.getPath(), 1L));
+        CommentResponse response3 = create(new CommentCreateRequestV2(1L, "my comment3", response2.getPath(), 1L));
+
+        System.out.println("response1.getPath() = " + response1.getPath());
+        System.out.println("response1.getCommentId() = " + response1.getCommentId());
+        System.out.println("\tresponse2.getPath() = " + response2.getPath());
+        System.out.println("\tresponse2.getCommentId() = " + response2.getCommentId());
+        System.out.println("\t\tresponse3.getPath() = " + response3.getPath());
+        System.out.println("\t\tresponse3.getCommentId() = " + response3.getCommentId());
+    }
+
+    CommentResponse create(CommentCreateRequestV2 request) {
+        return restClient.post()
+                .uri("/v2/comments")
+                .body(request)
+                .retrieve()
+                .body(CommentResponse.class);
+    }
+
+    @Test
+    void read() {
+        CommentResponse response = restClient.get()
+                .uri("/v2/comments/{commentId}", 121936176845195474L)
+                .retrieve()
+                .body(CommentResponse.class);
+        System.out.println("response = " + response);
+    }
+
+    @Test
+    void delete() {
+        restClient.delete()
+                .uri("/v2/comments/{commentId}", 121936176845195474L)
+                .retrieve();
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class CommentCreateRequestV2 {
+        private Long articleId;
+        private String content;
+        private String parentPath;
+        private Long writerId;
+    }
+}
